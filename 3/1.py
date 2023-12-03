52f350527b7f723817669beb1981f6a13f72f364
Koert Hup <k.hup125@gmail.com>
2023-12-03 12:01:24 +0100

    day 3

--- 3/1.py
+++ 3/1.py
@@ -7,7 +7,7 @@ numbersFound = {}
 def leftMostIndex(l,s):
     i = s
     while l[i-1].isdigit():
-        if i-1>=0:i -= 1
+        if i-1>=0:i-=1
         else:break
     return i
 
@@ -28,16 +28,18 @@ def walkLeft(l,s,y):
 
 def walkLine(l,s,y):
     if s>0 and l[s-1].isdigit():walkLeft(l,s,y)
-    if (s==0 and l[s].isdigit()) or (l[s-1] == '.' and l[s].isdigit()):walkRight(l,s,y)
-    if l[s] == '.' and s<=len(l) and l[s+1].isdigit():walkRight(l,s+1,y)
+    if (s==0 and l[s].isdigit()) or (not l[s-1].isdigit() and l[s].isdigit()):walkRight(l,s,y)
+    if not l[s].isdigit() and s<=len(l) and l[s+1].isdigit():walkRight(l,s+1,y)
 
 def checkSurroundings(x, y):
+    #up
     if y > 0:walkLine(f[y-1],x,y-1)
-        
-    l = f[y]
-    if x-1 > 0 and l[x-1].isdigit(): walkLeft(l,x-1,y)
-    if x+1 < len(l) and l[x+1].isdigit():walkRight(l,x+1,y)
 
+    #row 
+    l = f[y]
+    walkLine(l,x,y)
+
+    #down
     if y < len(f):walkLine(f[y+1],x,y+1)
 
 for i,l in enumerate(f):
--- 3/2.py
+++ 3/2.py
@@ -1,2 +1,59 @@
-f = open("sample.txt","r").read()
+import re
 
+f = open("input.txt","r").read().split("\n")
+
+numbersFound = []
+
+def leftMostIndex(l,s):
+    i = s
+    while l[i-1].isdigit():
+        if i-1>=0:i-=1
+        else:break
+    return i
+
+def walkRight(l, s, y):
+    i = s 
+    key=(i,y)
+    number = ''
+    while l[i].isdigit():
+        number += l[i]
+        if i+1<len(l):i+=1
+        else:break
+    if number != '':
+        return number
+
+def walkLeft(l,s,y):
+    i = leftMostIndex(l,s-1)  
+    return walkRight(l,i,y)
+
+def walkLine(l,s,y):
+    numbers = []
+    if s>0 and l[s-1].isdigit():numbers.append(walkLeft(l,s,y))
+    if (s==0 and l[s].isdigit()) or (not l[s-1].isdigit() and l[s].isdigit()):numbers.append(walkRight(l,s,y))
+    if not l[s].isdigit() and s<=len(l) and l[s+1].isdigit():numbers.append(walkRight(l,s+1,y))
+    return numbers
+
+def checkSurroundings(x, y):
+    numbers = []
+    #up
+    if y > 0:numbers += [*walkLine(f[y-1],x,y-1)]
+
+    #row 
+    l = f[y]
+    numbers += [*walkLine(l,x,y)]
+
+    #down
+    if y < len(f):numbers += [*walkLine(f[y+1],x,y+1)]
+
+    if len(numbers) == 2:
+        numbersFound.append(int(numbers[0])*int(numbers[1]))
+
+for i,l in enumerate(f):
+    for j,e in enumerate(l):
+        if re.match("[\*]",e):
+            checkSurroundings(j,i)
+    
+sum = 0
+for i in numbersFound:
+    sum += i
+print(sum)
