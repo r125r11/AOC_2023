05bf76550cfa6dd9f8e7129c51d31f76e8ad3e79
Koert Hup <k.hup125@gmail.com>
2023-12-03 11:46:23 +0100

    output too low

--- 3/1.py
+++ 3/1.py
@@ -1,2 +1,52 @@
-f = open("sample.txt","r").read()
+import re
 
+f = open("input.txt","r").read().split("\n")
+
+numbersFound = {}
+
+def leftMostIndex(l,s):
+    i = s
+    while l[i-1].isdigit():
+        if i-1>=0:i -= 1
+        else:break
+    return i
+
+def walkRight(l, s, y):
+    i = s 
+    key=(i,y)
+    number = ''
+    while l[i].isdigit():
+        number += l[i]
+        if i+1<len(l):i+=1
+        else:break
+    if number != '':
+        numbersFound[key]=number
+
+def walkLeft(l,s,y):
+    i = leftMostIndex(l,s-1)  
+    walkRight(l,i,y)
+
+def walkLine(l,s,y):
+    if s>0 and l[s-1].isdigit():walkLeft(l,s,y)
+    if (s==0 and l[s].isdigit()) or (l[s-1] == '.' and l[s].isdigit()):walkRight(l,s,y)
+    if l[s] == '.' and s<=len(l) and l[s+1].isdigit():walkRight(l,s+1,y)
+
+def checkSurroundings(x, y):
+    if y > 0:walkLine(f[y-1],x,y-1)
+        
+    l = f[y]
+    if x-1 > 0 and l[x-1].isdigit(): walkLeft(l,x-1,y)
+    if x+1 < len(l) and l[x+1].isdigit():walkRight(l,x+1,y)
+
+    if y < len(f):walkLine(f[y+1],x,y+1)
+
+for i,l in enumerate(f):
+    for j,e in enumerate(l):
+        if re.match("[^0-9\.]",e):
+            checkSurroundings(j,i)
+    
+print(numbersFound)
+sum = 0
+for i in numbersFound:
+    sum += int(numbersFound[i])
+print(sum)
