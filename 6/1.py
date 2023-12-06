322847bfd79abaa4a1204f3819dc5395146c7e6b
Koert Hup <k.hup125@gmail.com>
2023-12-06 10:49:55 +0100

    Day 6 disgusting, slow, but it works

--- 6/1.py
+++ 6/1.py
@@ -1,6 +1,21 @@
-import time
+import time,math
 start_time = time.time()
 
 f = open("input.txt","r").read().split("\n")
+
+t = [int(d) for d in f[0].split(":")[1].split()]
+d = [int(n) for n in f[1].split(":")[1].split()]
+
+v = []
+for i in range(len(t)):
+    r = 0
+    for x in range(t[i]):
+        if (t[i]-x) * x > d[i]:r +=1
+    v.append(r)
+
+print(v)
+prod = 1
+for d in v: prod *= d
+print(prod)
 
 print("-------  %s seconds -------" % (time.time() - start_time))
--- 6/2.py
+++ 6/2.py
@@ -1,6 +1,25 @@
-import time
+import time,math
 start_time = time.time()
 
 f = open("input.txt","r").read().split("\n")
+
+t = [(d) for d in f[0].split(":")[1].split()]
+d = [(n) for n in f[1].split(":")[1].split()]
+
+n = ''
+for x in t:n +=x
+m = ''
+for x in d:m +=x
+
+t = int(n)
+d = int(m)
+
+v = []
+r = 0
+for i in range(t):
+    if (t-i) * i > d:
+        r +=1
+
+print(r)
 
 print("-------  %s seconds -------" % (time.time() - start_time))
